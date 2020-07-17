a9514633e3126c5ea7e87ee10271e95514d19d16
unknown <you@example.com>
2020-07-17 22:40:39 +0600

    js class

---
+++ Assignment.js
@@ -0,0 +1,48 @@
+//feettoMile
+
+function feettoMile(feet){
+
+   var mile=feet/5280;
+   return mile;
+   
+}
+//wood calculator
+function woodcalCulator (table,chair,bed){
+
+
+   var tablecount=table*1
+   var chaircount=chair*3
+   var bedcount=bed*5
+
+   var totalwood=tablecount+chaircount+bedcount;
+   return totalwood;
+
+}
+
+
+//brick calculator
+
+
+//tinyFriend.
+
+function tinyFriend(numbers){
+      var tiny=numbers[0];
+      for (var i=0; i<numbers.length;i++){
+
+       var frlist=numbers[i];
+       if(frlist.length<tiny.length){
+          tiny=frlist;
+         
+         }
+
+      }
+
+return tiny;
+
+}
+
+
+
+   
+
+
